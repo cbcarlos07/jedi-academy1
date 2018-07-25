9a88e793555df16a046952e457e8f7ee0c8be627
Carlos <cbcarlos08@gmail.com>
2018-07-25 12:19:02 -0400

    Seção 4 24. Usando o Decorator @Input

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'jad-studante',
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  name: string = 'Luke'
-  isJedi: boolean = false
+  @Input() name: string
+  @Input() isJedi: boolean
   constructor() { }
 
   ngOnInit() {
